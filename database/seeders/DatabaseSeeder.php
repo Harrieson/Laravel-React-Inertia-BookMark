906677969c5506b43f5d43a02bd652b78d202552
Harrieson <harriemburu@gmail.com>
2022-04-03 16:22:51 +0300

    laravel-ui

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Harrie Mburu',
+            'email' => 'harriemburu@a.co',
+            'password' => \bcrypt('password'),
+        ]);
     }
 }
